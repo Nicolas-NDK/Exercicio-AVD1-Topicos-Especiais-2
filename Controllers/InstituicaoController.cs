2ca36550380af2e9181b7a8d93813861a401c319
Nicolas-NDK <nickquintanilha84@gmail.com>
2023-04-11 22:14:44 -0300

    Trabalho AVD 1 TÓPICOS ESPECIAIS II

---
+++ Controllers/InstituicaoController.cs
@@ -0,0 +1,31 @@
+﻿using Apoio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apoio.Controllers
+{
+    
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+    {
+        new Instituicao()
+        {
+            Id = 1,
+            Name = "Casa",
+            Endereco = "Polonia"
+        },
+        new Instituicao()
+        {
+            Id=2,
+            Name = "Casarão",
+            Endereco = "Suiça"
+        }
+    };
+        public IActionResult Index()
+        {
+             return View(instituicoes);
+            
+            
+        }
+    }
+}
---
+++ Models/Instituicao.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Apoio.Models
+{
+    public class Instituicao
+    {
+        public long Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        
+        public string? Name { get; set; }
+        [DisplayName("Endereço")]
+
+        public string? Endereco { get; set; }
+    };
+    
+}
